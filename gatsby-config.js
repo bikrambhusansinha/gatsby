29c40f9ff2c5e610586e8aeb8f59ff9bb4c09f95
Bikram Bhusan Sinha <bikrambsinha@hotmail.com>
2021-05-31 20:21:27 +0530

    Added the plugin for MongoDB

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,5 +13,29 @@ module.exports = {
         icon: 'src/images/gatsby-icon.png',
       },
     },
+    {
+ // The name of the plugin
+ resolve: `gatsby-source-mongodb`,
+ options: {
+ // Name of the database and collection where are records reside
+ dbName: 'covidn19',
+ collection: 'covid19',
+ server: {
+ address: 'cluster0-shard-00-02.7c1ks.mongodb.net',
+ port: 27017
+ },
+ auth: {
+ user: 'admin',
+ password: 'Mongodb1!'
+ },
+ extraParams: {
+ replicaSet: 'Main-shard-0',
+ ssl: true,
+ authSource: 'admin',
+ retryWrites: true
+ },
+refetchInterval: 60,
+ }
+ },
   ],
 };
